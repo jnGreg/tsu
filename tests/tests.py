3ac10583865133ae28cf5cbca53ffbcbea95bc1d
jnGreg <janpgregor@gmail.com>
2022-12-03 15:15:41 +0100

    test: article id not found

--- tests/tests.py
+++ tests/tests.py
@@ -2,8 +2,7 @@ import pytest
 
 from flask_restful import abort
 
-articles = {
-0: """New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County, New York.
+articles = {0: """New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County, New York.
 A year later, she got married again in Westchester County, but to a different man and without divorcing her first husband.
 Only 18 days after that marriage, she got hitched yet again. Then, Barrientos declared "I do" five more times, sometimes only within two weeks of each other.
 In 2010, she married once more, this time in the Bronx. In an application for a marriage license, she stated it was her "first and only" marriage.
@@ -19,8 +18,7 @@ Any divorces happened only after such filings were approved. It was unclear whet
 The case was referred to the Bronx District Attorney\'s Office by Immigration and Customs Enforcement and the Department of Homeland Security\'s
 Investigation Division. Seven of the men are from so-called "red-flagged" countries, including Egypt, Turkey, Georgia, Pakistan and Mali.
 Her eighth husband, Rashid Rajput, was deported in 2006 to his native Pakistan after an investigation by the Joint Terrorism Task Force.
-If convicted, Barrientos faces up to four years in prison.  Her next court appearance is scheduled for May 18.""",
-1: """REST Architecture
+If convicted, Barrientos faces up to four years in prison.  Her next court appearance is scheduled for May 18.""", 1: """REST Architecture
 REST stands for representational state transfer and is a software architecture style that defines a pattern for client and server communications over a network. REST provides a set of constraints for software architecture to promote performance, scalability, simplicity, and reliability in the system.
 
 REST defines the following architectural constraints:
@@ -45,9 +43,7 @@ This URL allows you to access information about a specific GitHub user. You acce
 HTTP Methods
 REST APIs listen for HTTP methods like GET, POST, and DELETE to know which operations to perform on the web service’s resources. A resource is any data available in the web service that can be accessed and manipulated with HTTP requests to the REST API. The HTTP method tells the API which action to perform on the resource.
 
-While there are many HTTP methods, the five methods listed below are the most commonly used with REST APIs:""",
-
-2: """What NLP Can Do
+While there are many HTTP methods, the five methods listed below are the most commonly used with REST APIs:""", 2: """What NLP Can Do
 The best known natural language processing tool is GPT-3, from OpenAI, which uses AI and statistics to predict the next word in a sentence based on the preceding words. NLP practitioners call tools like this “language models,” and they can be used for simple analytics tasks, such as classifying documents and analyzing the sentiment in blocks of text, as well as more advanced tasks, such as answering questions and summarizing reports. Language models are already reshaping traditional text analytics, but GPT-3 was an especially pivotal language model because, at 10x larger than any previous model upon release, it was the first large language model, which enabled it to perform even more advanced tasks like programming and solving high school–level math problems. The latest version, called InstructGPT, has been fine-tuned by humans to generate responses that are much better aligned with human values and user intentions, and Google’s latest model shows further impressive breakthroughs on language and reasoning.
 
 For businesses, the three areas where GPT-3 has appeared most promising are writing, coding, and discipline-specific reasoning. OpenAI, the Microsoft-funded creator of GPT-3, has developed a GPT-3-based language model intended to act as an assistant for programmers by generating code from natural language input. This tool, Codex, is already powering products like Copilot for Microsoft’s subsidiary GitHub and is capable of creating a basic video game simply by typing instructions. This transformative capability was already expected to change the nature of how programmers do their jobs, but models continue to improve — the latest from Google’s DeepMind AI lab, for example, demonstrates the critical thinking and logic skills necessary to outperform most humans in programming competitions.
@@ -63,12 +59,12 @@ def read_article(article_id):
     return articles[article_id]
 
 
-def article_id_not_found(article_id):
-    return article_id == -1
+def article_id_not_found(a_id):
+    return a_id not in articles.keys()
 
 
 def test_answer():
     assert inc(4) == 5
     assert read_article(1) == articles[1]
-    assert article_id_not_found(-1) == abort(404, message="Article not found")
+    assert True if article_id_not_found(-1) else False
 
