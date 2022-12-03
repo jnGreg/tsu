58ea67d61f620fce16f562746cacf01b672ae441
jnGreg <janpgregor@gmail.com>
2022-12-03 14:47:51 +0100

    test: read article

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,8 @@
 import pytest
 
 from flask_restful import abort
+
+import texts
 from texts import articles
 
 
@@ -9,7 +11,7 @@ def inc(x):
 
 
 def read_article(article_id):
-    return articles[article_id]
+    return texts.articles[article_id]
 
 
 def article_id_not_found(article_id):
@@ -18,6 +20,5 @@ def article_id_not_found(article_id):
 
 def test_answer():
     assert inc(4) == 5
-    assert read_article(1) == articles[1]
+    assert read_article(1) == texts.articles[1]
     assert article_id_not_found(-1) == abort(404, message="Article not found")
-
