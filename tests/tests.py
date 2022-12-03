d608c353c4a8e84cd85fdf248a604bb2ca076ef4
jnGreg <janpgregor@gmail.com>
2022-12-03 14:32:36 +0100

    test: read chosen article

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,7 @@
 import pytest
 
 from flask_restful import abort
+from texts import articles
 
 
 def inc(x):
@@ -8,7 +9,7 @@ def inc(x):
 
 
 def read_article(article_id):
-    return article_id == 1
+    return articles[article_id] == 1
 
 
 def article_id_not_found(article_id):
@@ -17,30 +18,5 @@ def article_id_not_found(article_id):
 
 def test_answer():
     assert inc(4) == 5
-    assert read_article(1) == """REST Architecture
-    REST stands for representational state transfer and is a software architecture style that defines a pattern for client and server communications over a network. REST provides a set of constraints for software architecture to promote performance, scalability, simplicity, and reliability in the system.
-
-    REST defines the following architectural constraints:
-
-    Stateless: The server won’t maintain any state between requests from the client.
-    Client-server: The client and server must be decoupled from each other, allowing each to develop independently.
-    Cacheable: The data retrieved from the server should be cacheable either by the client or by the server.
-    Uniform interface: The server will provide a uniform interface for accessing resources without defining their representation.
-    Layered system: The client may access the resources on the server indirectly through other layers such as a proxy or load balancer.
-    Code on demand (optional): The server may transfer code to the client that it can run, such as JavaScript for a single-page application.
-    Note, REST is not a specification but a set of guidelines on how to architect a network-connected software system.
-
-    Remove ads
-    REST APIs and Web Services
-    A REST web service is any web service that adheres to REST architecture constraints. These web services expose their data to the outside world through an API. REST APIs provide access to web service data through public web URLs.
-
-    For example, here’s one of the URLs for GitHub’s REST API:
-
-    https://api.github.com/users/<username>
-    This URL allows you to access information about a specific GitHub user. You access data from a REST API by sending an HTTP request to a specific URL and processing the response.
-
-    HTTP Methods
-    REST APIs listen for HTTP methods like GET, POST, and DELETE to know which operations to perform on the web service’s resources. A resource is any data available in the web service that can be accessed and manipulated with HTTP requests to the REST API. The HTTP method tells the API which action to perform on the resource.
-
-    While there are many HTTP methods, the five methods listed below are the most commonly used with REST APIs:"""
+    assert read_article(1) == articles[1:]
     assert article_id_not_found(-1) == abort(404, message="Article not found")
