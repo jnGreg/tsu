6192f7a904e17bdb055df64ba76e2fe41753bdab
MaciekG <maciek.1998.g@gmail.com>
2023-01-18 22:03:05 +0100

    fix change host, port in app.run

--- main.py
+++ main.py
@@ -157,5 +157,5 @@ def get_full(art_id):
 
 
 if __name__ == "__main__":
-
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
